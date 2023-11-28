6814d15d08c48afda32a2bd2a12a60bd5af98425
misha <mrmishj@gmail.com>
2023-11-28 20:17:21 +0200

    OrderFiller + PizzaPrototypeRegistry demo

---
+++ src/main/java/com/teamworkcpp/pizzariasimulator/backend/services/PizzaPrototypeRegistry.java
@@ -0,0 +1,36 @@
+package com.teamworkcpp.pizzariasimulator.backend.services;
+
+import com.teamworkcpp.pizzariasimulator.backend.interfaces.IPizza;
+import com.teamworkcpp.pizzariasimulator.backend.interfaces.IPizzaPrototype;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PizzaPrototypeRegistry {
+    private PizzaPrototypeRegistry _instance;
+    private List<IPizzaPrototype> _pizzaPrototypes = new ArrayList<>();
+
+    private PizzaPrototypeRegistry() {
+
+    }
+
+    private PizzaPrototypeRegistry getInstance() {
+        if (_instance == null) {
+            _instance = new PizzaPrototypeRegistry();
+        }
+        return _instance;
+    }
+
+    public List<String> getItemsNames() {
+        return _pizzaPrototypes.stream().map(IPizzaPrototype::getName).collect(Collectors.toList());
+    }
+
+    public IPizza getByName(String name) {
+        return (IPizza) _pizzaPrototypes.stream().filter(p -> p.getName().equals(name)).findFirst().orElse(null);
+    }
+
+    public void addItem(IPizzaPrototype pizzaPrototype) {
+        _pizzaPrototypes.add(pizzaPrototype);
+    }
+}
