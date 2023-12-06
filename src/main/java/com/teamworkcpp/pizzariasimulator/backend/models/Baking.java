91e1b4fdc78b0e5bb8efaab94a48920e43313060
Sviat <100282957+Natelai@users.noreply.github.com>
2023-12-07 00:20:09 +0200

    add code aestetic
    refactored code

--- src/main/java/com/teamworkcpp/pizzariasimulator/backend/models/Baking.java
+++ src/main/java/com/teamworkcpp/pizzariasimulator/backend/models/Baking.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 public class Baking implements IPizzaStatus{
     IPizza context;
+
     @Override
     public void next() {
         IPizzaStatus status = new FillingAfterBaking();
--- src/main/java/com/teamworkcpp/pizzariasimulator/backend/models/Done.java
+++ src/main/java/com/teamworkcpp/pizzariasimulator/backend/models/Done.java
@@ -2,15 +2,12 @@ package com.teamworkcpp.pizzariasimulator.backend.models;
 import com.teamworkcpp.pizzariasimulator.backend.interfaces.*;
 public class Done implements IPizzaStatus{
     IPizza context;
-    @Override
-    public void next() {
-
-    }
 
     @Override
-    public void stop() {
+    public void next() { }
 
-    }
+    @Override
+    public void stop() { }
 
     @Override
     public void setContext(IPizza p) {
